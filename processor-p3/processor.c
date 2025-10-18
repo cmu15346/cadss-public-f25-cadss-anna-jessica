7871dc92e64a454bae2b6382ad944333d5952ad2
jgli <me@jessica.li>
2025-10-18 06:45:22 -0400

    caffeine <3

--- processor-p3/processor.c
+++ processor-p3/processor.c
@@ -18,27 +18,27 @@
 uint64_t F, D, M, J, K, C = 0;
 
 // unique tag counter
-uint64_t counter = 0;
+uint64_t counter = 1;
 
 typedef struct reg_ {
     bool ready;
     uint32_t tag;
     uint32_t val;
-    uint32_t reg_id;
+    int reg_id;
 } reg;
 
 typedef struct CDB_ {
     bool busy;
     uint32_t tag;
     uint32_t val;
-    uint32_t reg_id;
+    int reg_id;
 } CDB;
 
 typedef struct instr_ {
     bool is_long;
     bool fired;
     uint32_t FU;
-    uint32_t dest;
+    int dest;
     reg *src_arr[2];
     uint32_t tag;
 } instr;
@@ -53,12 +53,18 @@ instr *init_instr(bool is_long, uint32_t dest, int srcs[]) {
             I->src_arr[i]->reg_id = srcs[i];
         }
     }
+    I->tag = counter;
+    counter++;
     return I;
 }
 
+// 33 registers
 reg *regs = NULL;
+// C buses
 CDB *buses = NULL;
-instr **FU_pipeline = NULL;
+// 2d array of instr*, first J are fast, next K are long
+instr ***FU_pipeline = NULL;
+
 
 typedef struct instr_node_ {
     instr *instr;
@@ -83,8 +89,8 @@ instr_queue *init_queue(int cap) {
 
 instr_queue *decode_queue = NULL;
 instr_queue *dispatch_queue = NULL;
-instr_queue *long_schedule_queue = NULL;
-instr_queue *fast_schedule_queue = NULL;
+instr_queue *long_schedule_queue = NULL; // priority queue based on tag
+instr_queue *fast_schedule_queue = NULL; // priority queue based on tag
 instr_queue *state_update_queue = NULL; // priority queue based on tag
 
 trace_reader *tr = NULL;
@@ -176,6 +182,37 @@ instr *queue_pop(instr_queue *q) {
     return ret;
 }
 
+bool queue_delete(instr_queue *q, instr* v) {
+    if (queue_empty(q))
+        return false;
+    instr_node *prev = NULL;
+    instr_node* cur = q->head;
+    while (cur != NULL && cur->instr != v) {
+        prev = cur;
+        cur = cur->next;
+    }
+    // if not found, return false
+    if (cur->instr != v) 
+        return false;
+
+    // completely middle case
+    if (cur != q->head && cur != q->tail) {
+        prev->next = cur->next;
+    }
+    else {
+        if (cur == q->head) {
+            q->head = cur->next;
+        }
+        if (cur == q->tail) {
+            q->tail = prev;
+            if (prev != NULL) prev->next = NULL;
+        }
+    }
+    free(cur);
+    q->cnt--;
+    return true;
+}
+
 instr *queue_peek(instr_queue *q) {
     if (queue_empty(q)) {
         return NULL;
@@ -185,12 +222,14 @@ instr *queue_peek(instr_queue *q) {
 
 int find_CDB_by_tag(uint32_t tag) {
     for (int i = 0; i < C; i++) {
-        if (buses[i].tag == tag) {
+        // busy == broadcasting
+        if (buses[i].busy && buses[i].tag == tag) {
             return i;
         }
     }
     return -1;
 }
+
 
 //
 // init
@@ -239,8 +278,16 @@ processor *init(processor_sim_args *psa) {
     }
 
     regs = calloc(33, sizeof(reg));
+    // all registers init as ready b/c lmao
+    for (int i = 0; i < 33; i++) {
+        regs[i].ready = true;
+    }
     buses = calloc(C, sizeof(CDB));
-    FU_pipeline = calloc(3 * (J + K), sizeof(instr *));
+    FU_pipeline = calloc(J + K, sizeof(instr**));
+    for (int i = 0; i < J + K; i++) {
+        instr** instr_arr = calloc(3, sizeof(instr*));
+        FU_pipeline[i] = instr_arr;
+    }
 
     decode_queue = init_queue(UINT32_MAX);
     dispatch_queue = init_queue(D * (M * J + M * K));
@@ -302,64 +349,177 @@ int tick(void) {
 
     int progress = 0;
     for (int i = 0; i < processorCount; i++) {
-        if (pendingMem[i] == 1) {
-            progress = 1;
-            continue;
+//      regs_print("start of tick");
+//      cdbs_print("start of tick");
+        // The register file is written via the result bus. (SU g + set CDB.busy to false)
+        for (int j = 0; j < C; j++) {
+            if (buses[j].busy) {
+                if (buses[j].reg_id >= 0) {
+                    if (regs[buses[j].reg_id].tag == buses[j].tag) {
+                        regs[buses[j].reg_id].ready = true;
+                        regs[buses[j].reg_id].val = buses[j].val;
+                    }
+                }
+                buses[j].busy = false;
+                DPRINTF("progress = 1 reg <- result bus\n");
+                progress = 1;
+            }
         }
 
-        // In the full processor simulator, the branch is pending until
-        //   it has executed.
-        if (pendingBranch[i] > 0) {
-            pendingBranch[i]--;
+        instr* completed[4] = {NULL, NULL, NULL, NULL};
+        // The state update unit pulls from the state update queue and updates the 
+        // result bus (SU a-e)
+        for (int c = 0; c < C; c++) {
+            if (queue_empty(state_update_queue)) break;
+            instr* I = queue_pop(state_update_queue);
+            buses[c].busy = true;
+            buses[c].tag = I->tag;
+            // TODO: replace DUMMY VALUE? or ok?
+            buses[c].val = 0;
+            buses[c].reg_id = I->dest;
+            // Scoreboard[FU].busy = false already done by setting FU_pipeline[j][0] null
+
+            // mark as completed
+            completed[c] = I;
+            DPRINTF("progress = 1 SU a-e, %p\n", I);
             progress = 1;
-            continue;
         }
+
+        // — END: STATE UPDATE LATCH —
+
+        // — START: EXECUTE LATCH —
+
+//      for (int j = 0; j < J + K; j++) {
+//          for (int k = 0; k < 3; k++) {
+//              DPRINTF("FU_pipeline[%d][%d] = %p\n", j,k,FU_pipeline[j][k]);
+//          }
+//      }
+//      DPRINTF("ignore\n");
+
+        // advance FU_pipeline[j] - smth should complete for progress
+        for (int j = 0; j < J + K; j++) {
+            instr* to_queue = NULL;
+            // fast
+            if (j < J) {
+                to_queue = FU_pipeline[j][0];
+                FU_pipeline[j][0] = NULL;
+            }
+            // long
+            else {
+                to_queue = FU_pipeline[j][2];
+                FU_pipeline[j][2] = FU_pipeline[j][1];
+                FU_pipeline[j][1] = FU_pipeline[j][0];
+                FU_pipeline[j][0] = NULL;
+            }
+            // put completed instructions in state_update_queue
+//          DPRINTF("to_queue: %p\n", to_queue);
+            if (to_queue != NULL) {
+                DPRINTF("priority push %p into state update quueue\n", to_queue);
+                priority_push(state_update_queue, to_queue);
+                DPRINTF("progress = 1 state update queue push\n");
+                progress = 1;
+            }
+        }
+        for (int j = 0; j < J + K; j++) {
+            for (int k = 0; k < 3; k++) {
+                DPRINTF("FU_pipeline[%d][%d] = %p\n", j,k,FU_pipeline[j][k]);
+                if (FU_pipeline[j][k] != NULL) {
+                    DPRINTF("progress = 1 execute pipeline moved\n");
+                    progress = 1;
+                }
+            }
+        }
+
+
+        // — END: EXECUTE LATCH —
+
+        // — START: SCHEDULE/DISPATCH LATCH —
 
         // schedule b: mark indep instructions in schedule queue to fire
-        instr_node *cur_node = long_schedule_queue->head;
-        while (cur_node != NULL) {
-            instr *RS = cur_node->instr;
+        instr_queue* qs[2] = {long_schedule_queue, fast_schedule_queue};
+        for (int j = 0; j < 2; j++) {
+            instr_queue* q = qs[j];
+            instr_node *cur_node = q->head;
+            while (cur_node != NULL) {
+                instr *RS = cur_node->instr;
 
-            // TODO: wakeup schedule b
-
-            cur_node = cur_node->next;
+                if (!RS->fired) {
+                    bool ready = true;
+                    for (int j = 0; j < 2; j++) {
+                        if (RS->src_arr[j] != NULL && !RS->src_arr[j]->ready) ready = false;
+                    }
+                    if (ready) {
+                        // find next possible FU
+                        for (int j = 0; j < J + K; j++) {
+                            if (j < J && RS->is_long) continue;
+                            if (j >= J && !RS->is_long) continue;
+                            if (FU_pipeline[j][0] == NULL) {
+                                FU_pipeline[j][0] = RS;
+                                RS->FU = j;
+                                RS->fired = true;
+                                DPRINTF("progress = 1 scheduling b\n");
+                                progress = 1;
+                                break;
+                            }
+                        }
+                    }
+                }
+                cur_node = cur_node->next;
+            }
         }
 
-        // instruction move (decode_queue -> dispatch_queue)
-        int fetch_cnt = 0;
-        while (!queue_empty(decode_queue) && !queue_full(dispatch_queue) &&
-               fetch_cnt < F) {
-            instr *decoded = queue_pop(decode_queue);
-            queue_push(dispatch_queue, decoded);
-            fetch_cnt++;
-            progress = 1;
-        }
 
         // dispatch unit reserves slots in scheduling queues
-        while (!queue_empty(dispatch_queue)) {
-            instr *cur_instr = queue_peek(dispatch_queue);
+        instr_node* cur_node = dispatch_queue->head;
+        while (cur_node != NULL) {
+            if (queue_full(long_schedule_queue) && queue_full(fast_schedule_queue)) {
+                DPRINTF("schedule queues full\n");
+                break;
+            }
+            instr *cur_instr = cur_node->instr;
+            instr_node* next = cur_node->next;
             // a: add I to first free slot of schedule queue
             if (cur_instr->is_long) {
-                if (queue_full(long_schedule_queue))
-                    break;
-                // dispatch if schedule queue not full
+                if (queue_full(long_schedule_queue)) {
+                    DPRINTF("long schedule queue full\n");
+                    cur_node = next;
+                    continue;
+                }
+                DPRINTF("push %p into long schedule quueue\n", cur_instr);
                 queue_push(long_schedule_queue, cur_instr);
             } else {
-                if (queue_full(fast_schedule_queue))
-                    break;
-                // dispatch if schedule queue not full
-                queue_push(fast_schedule_queue, cur_instr);
-            }
-            // b: delete I from dispatch queue
-            queue_pop(dispatch_queue);
-
-            // TODO: do we need to move e-f into its own loop?
-            // e: for all src registers i of I, do:
-            for (int i = 0; i < 2; i++) {
-                reg *src = cur_instr->src_arr[i];
-
-                if (src == NULL)
+                if (queue_full(fast_schedule_queue)) {
+                    DPRINTF("fast schedule queue full\n");
+                    cur_node = next;
                     continue;
+                }
+//              queue_print("fast before", fast_schedule_queue);
+                DPRINTF("push %p into fast schedule quueue\n", cur_instr);
+//              queue_print("fast after", fast_schedule_queue);
+                queue_push(fast_schedule_queue, cur_instr);
+                // dispatch if schedule queue not full
+            }
+            DPRINTF("progress = 1 dispatch_queue reserve\n");
+            progress = 1;
+            // b: delete I from dispatch queue
+            assert(queue_delete(dispatch_queue, cur_instr));
+            cur_node = next;
+
+            // e: for all src registers i of I, do:
+            // if either of regs are -1 (NULL), srcs both become 0
+            bool cleared = cur_instr->src_arr[0] == NULL ||
+                                    cur_instr->src_arr[1] == NULL;
+
+            for (int i = 0; i < 2; i++) {
+                if (cleared) {
+                    if (cur_instr->src_arr[i] == NULL) {
+                        cur_instr->src_arr[i] = calloc(1, sizeof(reg));
+                    }
+                    cur_instr->src_arr[i]->reg_id = 0;
+                }
+
+                reg *src = cur_instr->src_arr[i];
+                assert(src != NULL);
 
                 if (regs[src->reg_id].ready) {
                     cur_instr->src_arr[i]->val = regs[src->reg_id].val;
@@ -370,18 +530,50 @@ int tick(void) {
                 }
             }
 
-            // f-g: tag the destination
-            regs[cur_instr->dest].tag = counter;
-            cur_instr->tag = regs[cur_instr->dest].tag;
-            counter++;
-            // h: set register unready, will be ready once execution completes
-            regs[cur_instr->dest].ready = false;
-
-            progress = 1;
+            // f-g: tag the destination -- if it is a register
+            if (cur_instr->dest >= 0) {
+                regs[cur_instr->dest].tag = cur_instr->tag;
+                // h: set register unready, will be ready once execution completes
+                regs[cur_instr->dest].ready = false;
+            }
         }
 
-        // instruction fetch (read into decode queue)
+        // schedule a: scheduling queues updated from result buses
+        for (int j = 0; j < 2; j++) {
+            instr_queue* q = qs[j];
+            cur_node = q->head;
+            while (cur_node != NULL) {
+                instr *RS = cur_node->instr;
+                
+                for (int i = 0; i < 2; i++) {
+                    reg *src = RS->src_arr[i];
+
+                    // bc -1 means both srcs are 0 .... ;-;
+                    assert(src != NULL);
+
+                    if (!src->ready){
+                        int CDB = find_CDB_by_tag(src->tag);
+                        if (CDB != -1) {
+                            src->ready = true;
+                            src->val = buses[CDB].val;
+                            DPRINTF("progress = 1 scheduling a\n");
+                            progress = 1;
+                        }
+                    }
+                }
+
+                cur_node = cur_node->next;
+            }
+        }
+
+        // — END: SCHEDULE/DISPATCH LATCH —
+
+        // — START: I F/D LATCH —
         for (uint64_t f = 0; f < F; f++) {
+            if (queue_full(dispatch_queue)) {
+                // if dispatch queue is full, halts
+                break;
+            }
 
             // get and manage ops for each processor core
             nextOp = tr->getNextOp(i);
@@ -389,56 +581,55 @@ int tick(void) {
             if (nextOp == NULL)
                 continue;
 
+            DPRINTF("progress = 1 instruction\n");
             progress = 1;
 
             switch (nextOp->op) {
-            case MEM_LOAD:
-            case MEM_STORE:
-                pendingMem[i] = 1;
-                cs->memoryRequest(nextOp, i, makeTag(i, memOpTag[i]),
-                                  memOpCallback);
-                break;
+//          case MEM_LOAD:
+//          case MEM_STORE:
+//              pendingMem[i] = 1;
+//              cs->memoryRequest(nextOp, i, makeTag(i, memOpTag[i]),
+//                                memOpCallback);
+//              break;
 
-            case BRANCH:
-                pendingBranch[i] =
-                    (bs->branchRequest(nextOp, i) == nextOp->nextPCAddress) ? 0
-                                                                            : 1;
-                break;
+//          case BRANCH:
+//              pendingBranch[i] =
+//                  (bs->branchRequest(nextOp, i) == nextOp->nextPCAddress) ? 0
+//                                                                          : 1;
+//              break;
 
             case ALU:
             case ALU_LONG:;
                 instr *new_instr = init_instr(
                     nextOp->op == ALU_LONG, nextOp->dest_reg, nextOp->src_reg);
-                queue_push(decode_queue, new_instr);
-
+                DPRINTF("push %p into dispatch queue\n", new_instr);
+                assert(queue_push(dispatch_queue, new_instr));
+//              queue_print("dispatch_queue", dispatch_queue);
                 break;
             }
 
             free(nextOp);
         }
+        // — END: I F/D LATCH —
+        
+        // — START: STATE UPDATE LATCH —
 
-        // schedule a: scheduling queues updated from result buses
-        cur_node = long_schedule_queue->head;
-        while (cur_node != NULL) {
-            instr *RS = cur_node->instr;
-
-            for (int i = 0; i < 2; i++) {
-                reg *src = RS->src_arr[i];
-
-                if (src == NULL)
-                    continue;
-
-                int CDB = find_CDB_by_tag(src->tag);
-                if (CDB != -1) {
-                    src->ready = true;
-                    src->val = buses[CDB].val;
-                }
+        // The state update unit deletes completed instructions 
+        // from the scheduling queue. (SU f)
+        for (int c = 0; c < C; c++) {
+            instr* del_instr = completed[c];
+            if (del_instr != NULL) {
+                DPRINTF("progress = 1 su f\n");
+                progress = 1;
+                instr_queue *q = del_instr->is_long ? 
+                    long_schedule_queue : fast_schedule_queue;
+                assert(queue_delete(q, del_instr));
+                free(del_instr);
             }
-
-            cur_node = cur_node->next;
         }
     }
 
+//  printf("progress: %d, ticks... %ld\n", progress, tickCount);
     return progress;
 }
 
